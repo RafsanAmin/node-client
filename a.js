b1df5db106f457c9bbd6cf5bba97d8f0d50e8686
hrmrafsan <59786405+RafsanAmin@users.noreply.github.com>
2021-04-19 14:00:51 +0600

    Add files via upload

--- a.js
+++ a.js
@@ -2,16 +2,35 @@ $("#haha").click(function () {
   $("div > div").html("");
 });
 
+let authe = 0;
+
 getData({});
 
+function auth() {
+  authe = $(".auth").val();
+  $.ajax({
+    type: "GET",
+    url: "https://rafsanamin.herokuapp.com/db/",
+    headers: { pass: authe },
+    success: function (data) {
+      let u = data;
+      if (data !== "forbidden") {
+        u = "connected";
+      }
+      $(".authc").empty();
+      $(".authc").append(`<h5>You are ${u}</h5>`);
+    },
+  });
+  getData({});
+}
 function getData(clue) {
   let send = clue;
-
   $.ajax({
     type: "GET",
     url: "https://rafsanamin.herokuapp.com/db/",
     data: send,
-    dataType: "json",
+    contentType: "application/json",
+    headers: { pass: authe },
     success: function (data) {
       $(".data-table").empty();
       data.forEach((elem) => {
@@ -30,8 +49,8 @@ function postData() {
     type: "POST",
     url: "https://rafsanamin.herokuapp.com/db",
     data: send,
-    dataType: "json",
-    success: function (data, textStatus, jQxhr) {
+    headers: { pass: authe },
+    success: function (data) {
       $(".added").html(`<h5>Your name is ${data.ma}</h5>`);
       $("input").val("");
       getData({});
@@ -49,6 +68,7 @@ function putData() {
     type: "PUT",
     url: "https://rafsanamin.herokuapp.com/db",
     data: { sent: send },
+    headers: { pass: authe },
     success: function (data) {
       if (data == null) {
         $(".change-status").html(`<h5> Data Not found and updated`);
@@ -68,6 +88,7 @@ function delData(elem) {
     type: "DELETE",
     url: "https://rafsanamin.herokuapp.com/db",
     data: send,
+    headers: { pass: authe },
     success: function () {
       getData({});
     },
